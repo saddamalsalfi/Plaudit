096e236cb31214c61910b69c7fe1fa44460d076b
Saddam Al-Slfi <saddamalsalfi@gmail.com>
2025-08-24 02:40:54 +0300

    Add files via upload

---
+++ PlauditPlugin.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * @file plugins/generic/plaudit/PlauditPlugin.php
+ *
+ * Copyright (c) 2022 - 2024 Lepidus Tecnologia
+ * Distributed under the GNU GPL v3. For full terms see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt.
+ *
+ * @class PlauditPlugin
+ * @ingroup plugins_generic_plauditPlugin
+ *
+ * @brief Plaudit Plugin
+ */
+
+namespace APP\plugins\generic\plaudit;
+
+use PKP\plugins\GenericPlugin;
+use APP\core\Application;
+use PKP\plugins\Hook;
+use PKP\linkAction\LinkAction;
+use PKP\linkAction\request\AjaxModal;
+use PKP\core\JSONMessage;
+use APP\plugins\generic\plaudit\form\PlauditSettingsForm;
+
+class PlauditPlugin extends GenericPlugin
+{
+    public function register($category, $path, $mainContextId = null)
+    {
+        $success = parent::register($category, $path, $mainContextId);
+
+        if (Application::isUnderMaintenance()) {
+            return true;
+        }
+
+        if ($success && $this->getEnabled($mainContextId)) {
+            Hook::add('Templates::Preprint::Details', [$this, 'addSubmissionDetails']);
+            Hook::add('Templates::Catalog::Book::Details', [$this, 'addSubmissionDetails']);
+            Hook::add('Templates::Article::Details', [$this, 'addSubmissionDetails']);
+        }
+
+        return $success;
+    }
+
+    public function getDisplayName()
+    {
+        return __('plugins.generic.plaudit.displayName');
+    }
+
+    public function getDescription()
+    {
+        return __('plugins.generic.plaudit.description');
+    }
+
+    public function addSubmissionDetails($hookName, $params)
+    {
+        $templateMgr = $params[1];
+        $output = & $params[2];
+
+        $request = Application::get()->getRequest();
+        $integrationToken = $this->getSetting($request->getContext()->getId(), 'integration_token');
+
+        if ($integrationToken) {
+            $templateMgr->assign('integrationToken', $integrationToken);
+            $output .= $templateMgr->fetch($this->getTemplateResource('plauditWidget.tpl'));
+        }
+
+        return false;
+    }
+
+    public function getActions($request, $actionArgs)
+    {
+        $actions = parent::getActions($request, $actionArgs);
+
+        if (!$this->getEnabled()) {
+            return $actions;
+        }
+
+        $router = $request->getRouter();
+$linkAction = new LinkAction(
+            'settings',
+            new AjaxModal(
+                $router->url(
+                    $request,
+                    null,
+                    null,
+                    'manage',
+                    null,
+                    array(
+                        'verb' => 'settings',
+                        'plugin' => $this->getName(),
+                        'category' => 'generic'
+                    )
+                ),
+                $this->getDisplayName()
+            ),
+            __('manager.plugins.settings'),
+            null
+        );
+
+        array_unshift($actions, $linkAction);
+
+        return $actions;
+    }
+
+    public function manage($args, $request)
+    {
+        switch ($request->getUserVar('verb')) {
+            case 'settings':
+                $context = $request->getContext();
+                $form = new PlauditSettingsForm($this, $context->getId());
+
+                if ($request->getUserVar('save')) {
+                    $form->readInputData();
+                    if ($form->validate()) {
+                        $form->execute();
+                        return new JSONMessage(true);
+                    }
+                }
+
+                return new JSONMessage(true, $form->fetch($request));
+            default:
+                return parent::manage($verb, $args, $message, $messageParams);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# OJS Plaudit Plugin
-OJS Plaudit Plugin
+# Plaudit Plugin 
+
+This plugin adds the [Plaudit](https://plaudit.pub/) widget to the submission details in the public site.
+
+An integration token is required in order for the plugin to work properly. [See how to get one](#obtaining-an-integration-token)
+
+## Compatibility
+
+This release has been tested with:
+
+* OJS 3.5.0
+* OJS 3.4.0
+* OPS 3.4.0
+* OMP 3.4.0
+
+## Installation
+
+1. Enter the administration area of ​​your OPS website through the __Dashboard__.
+2. Navigate to `Website Settings` > `Plugins` > `Installed Plugins` > `Upload a new plugin`.
+3. Under __Upload file__ select the file __plaudit.tar.gz__, downloaded from the latest release.
+4. Click __Save__ and the plugin will be installed on your website.
+
+## Configure your Plaudit integration token
+
+1. After the plugin is installed and enabled, open the plugin settings:
+
+    ![plaudit-plugin-page](assets/plaudit-plugin-page.png)
+
+2. In the indicated field, paste your Plaudit integration token, and save the form.
+    
+    ![plaudit-settings-page](assets/plaudit-settings-page.png)
+
+3. All good! You should see the Plaudit widget in the submissions details.
+
+    ![plaudit-widget](assets/plaudit-widget.png)
+
+## Obtaining an integration token
+
+If you dont have one already, you can folow the [Plaudit instructions on how to obtain it](https://plaudit.pub/integration):
+
+*"To make sure Plaudit can handle the load, we are gradually rolling out integrations. Thus, at this point in time it is required to [contact us](mailto:integrate@plaudit.pub) if you are interested in integrating the Plaudit widget into your website. When your integration is approved, we will ask you for the host names you want to display the widget at (e.g. `example.com` and `dev.example.com`). You will then receive a token that can be used to integrate the widget at thost host names."*
+
+---
+
+# License
+__This plugin is licensed under the GNU General Public License v3.0__
+
+__Copyright (c) 2022-2024 Lepidus Tecnologia__
